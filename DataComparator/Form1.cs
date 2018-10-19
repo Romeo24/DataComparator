6ed882b53ad6a73cdbc74eb919057ba030f6931f
Romeo24 <stabronyk@gmail.com>
2018-10-19 20:51:29 +0300

    Доопрацьоване меню діалогового вікна вибору Ексель-файла

--- DataComparator/Form1.cs
+++ DataComparator/Form1.cs
@@ -22,13 +22,14 @@ namespace DataComparator
             using (OpenFileDialog ofd = new OpenFileDialog())
             {
                 ofd.InitialDirectory = "C:\\";
-                ofd.Filter = "Excel-файли (*.xls)|*.xls";
-                ofd.FilterIndex = 2;
+                ofd.DefaultExt = "*.xls;*.xlsx";
+                ofd.Title = "Вибір Excel-файла для імпорту";
+                ofd.Filter = "Excel 97-2003 (*.xls)|*.xls|Excel 2007-2010 (*.xlsx)|*.xlsx";
+                ofd.FilterIndex = 1;
                 ofd.RestoreDirectory = true;
                 if (ofd.ShowDialog() == DialogResult.OK)
                     tbx_FilePath.Text = ofd.FileName;
             };
-
         }
     }
 }
